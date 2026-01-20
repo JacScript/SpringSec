3387fcd24d7c3e426ab599382386ac99a7dac8a4
JacScript <jacksonsylas59@gmail.com>
2026-01-20 22:01:36 +0300

    refactor(controller): remove unused RequestParam import
    Remove unused import statement for RequestParam annotation from StudentController to clean up dependencies and improve code maintainability.

--- src/main/java/com/heritage/SpringSec/StudentController.java
+++ src/main/java/com/heritage/SpringSec/StudentController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 public class StudentController {
